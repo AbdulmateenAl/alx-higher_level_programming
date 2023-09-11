934fb5ff3bb79d8a8bef39afdadfde098bd61d4b
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2023-09-11 02:23:19 -0700

    was in a wrong directory

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+lineList = ["C is fun", "Python is cool", "JavaScript is amazing"];
+length = lineList.length;
+
+var i = 0;
+while (i < length) {
+	console.log(lineList[i]);
+	i += 1;
+}

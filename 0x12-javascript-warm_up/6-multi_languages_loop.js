dee85326d5c0fdf32725376dad54449d41d9f901
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2023-09-16 04:50:06 -0700

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-lineList = ["C is fun", "Python is cool", "JavaScript is amazing"];
-length = lineList.length;
+const lineList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const length = lineList.length;
 
-var i = 0;
-while (i < length) {
-	console.log(lineList[i]);
-	i += 1;
+for (let i = 0; i < length; i++) {
+  console.log(lineList[i]);
 }
